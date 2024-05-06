18905cac0f37b0ab212d848becbab0969426b8b0
nurislamkalmanbetov <nurislam_96@mail.ru>
2024-05-06 14:43:35 +0600

    add jwt token, login, signup

--- auth_routes.py
+++ auth_routes.py
@@ -19,55 +19,108 @@ session = Session(bind=engine)
 
 
 @auth_router.get('/')
-async def hello():
-    return {"message":"Hello World - auth router"}
+async def hello(Authorize:AuthJWT=Depends()):
+
+    """
+        ## Sample hello world route
+    
+    """
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+
+    return {"message":"Hello World"}
 
 
-
-@auth_router.post('/signup',response_model=SignUpModel,
-    status_code=status.HTTP_201_CREATED                  
-)
+@auth_router.post('/signup',response_model=SignUpModel,status_code=status.HTTP_201_CREATED)
 async def signup(user:SignUpModel):
-    db_email = session.query(User).filter(User.email==user.email).first()
+    db_email=session.query(User).filter(User.email==user.email).first()
+
     if db_email is not None:
         return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-            details="User with the email already exists"                     
+            detail="User with the email already exists"
         )
-    
-    db_username = session.query(User).filter(User.username==user.username).first()
+
+    db_username=session.query(User).filter(User.username==user.username).first()
+
     if db_username is not None:
         return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-            details="User with the username already exists"                     
+            detail="User with the username already exists"
         )
-    
+
     new_user=User(
         username=user.username,
         email=user.email,
         password=generate_password_hash(user.password),
         is_active=user.is_active,
         is_staff=user.is_staff
-        )
-    
+    )
+
     session.add(new_user)
+
     session.commit()
+
     return new_user
 
 
-# login route
-@auth_router.post('/login', status_code=200)
-async def login(user: LoginModel, Authorize: AuthJWT = Depends()):
-    db_user = session.query(User).filter(User.username == user.username).first()
-    if db_user and check_password_hash(db_user.password, user.password):
-        access_token = Authorize.create_access_token(subject=db_user.username)
-        refresh_token = Authorize.create_refresh_token(subject=db_user.username)
 
-        response = {
-            "access": access_token,
-            "refresh": refresh_token
+#login route
+
+@auth_router.post('/login',status_code=200)
+async def login(user:LoginModel,Authorize:AuthJWT=Depends()):
+    """     
+        ## Login a user
+        This requires
+            ```
+                username:str
+                password:str
+            ```
+        and returns a token pair `access` and `refresh`
+    """
+    db_user=session.query(User).filter(User.username==user.username).first()
+
+    if db_user and check_password_hash(db_user.password, user.password):
+        access_token=Authorize.create_access_token(subject=db_user.username)
+        refresh_token=Authorize.create_refresh_token(subject=db_user.username)
+
+        response={
+            "access":access_token,
+            "refresh":refresh_token
         }
 
         return jsonable_encoder(response)
 
-    # Возвращаем более информативное сообщение об ошибке
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                        detail="Invalid username or password")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Invalid Username Or Password"
+    )
+
+
+
+#refreshing tokens
+
+@auth_router.get('/refresh')
+async def refresh_token(Authorize:AuthJWT=Depends()):
+    """
+    ## Create a fresh token
+    This creates a fresh token. It requires an refresh token.
+    """
+
+
+    try:
+        Authorize.jwt_refresh_token_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Please provide a valid refresh token"
+        ) 
+
+    current_user=Authorize.get_jwt_subject()
+
+    
+    access_token=Authorize.create_access_token(subject=current_user)
+
+    return jsonable_encoder({"access":access_token})
--- schemas.py
+++ schemas.py
@@ -3,13 +3,12 @@ from typing import Optional
 
 
 class SignUpModel(BaseModel):
-    id:Optional[int]
-    username:str
-    email:str
-    password:str
-    is_staff:Optional[bool]
-    is_active:Optional[bool]
-
+    id: Optional[int]
+    username: str
+    email: str
+    password: str
+    is_staff: Optional[bool]
+    is_active: Optional[bool]
 
     class Config:
         orm_mode=True
